3980d488fac6e83657ac075aafb43338fa944a13
dumon <dumono@gmail.com>
2021-12-16 21:51:41 +0300

    добавление класса GPIO

---
+++ lib/GPIO.py
@@ -0,0 +1,19 @@
+class GPIO(object):
+    pins = {}
+    def __init__(self, count):
+        DEFAULT_PIN_NAME="DFP"
+        DEFAULT_PIN_DESC="DESC"
+        for i in range(count):
+            self.pins[i+1] = {'pin': DEFAULT_PIN_NAME, 'desc': DEFAULT_PIN_DESC }
+
+    def get_pin_name(self, pin):
+        return self.pins[pin]['pin']
+
+    def set_pin_name(self, pin, value):
+        self.pins[pin] = {'pin': value, 'desc': self.pins[pin]['desc']}
+
+    def get_pin_desc(self, pin):
+        return self.pins[pin]['desc']
+
+    def set_pin_desc(self, pin, value):
+        self.pins[pin] = {'pin': self.pins[pin]['pin'], 'desc': value}
